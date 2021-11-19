4b945064c3554c52e58b2a92c323b35ed061521b
zhangsan <1513157458@qq.com>
2021-11-19 19:26:31 +0800

    4811174502841027019385211

---
+++ lc经典题目/344.c
@@ -0,0 +1,23 @@
+/*
+    对于长度为 N 的待被反转的字符数组，我们可以观察反转前后下标的变化，
+    假设反转前字符数组为 s[0] s[1] s[2] ... s[N - 1]，
+    那么反转后字符数组为 s[N - 1] s[N - 2] ... s[0]。
+    比较反转前后下标变化很容易得出 s[i] 的字符与 s[N - 1 - i] 的字符发生了交换的规律，
+    因此我们可以得出如下双指针的解法：
+        将 left 指向字符数组首元素，right 指向字符数组尾元素。
+        当 left < right：
+        交换 s[left] 和 s[right]；
+        left 指针右移一位，即 left = left + 1；
+        right 指针左移一位，即 right = right - 1。
+        当 left >= right，反转结束，返回字符数组即可。
+*/
+
+void reverseString(char* s, int sSize){
+    for (int left = 0, right = sSize - 1; left < right; left++, right--) {
+        int temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+    }
+
+    return 0;
+}
