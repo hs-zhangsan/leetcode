8f268383c3af1dfb36e532f7b7815e936ee387b3
hs-zhangsan <1513157458@qq.com>
2021-12-21 07:37:09 +0800

    4811174502841027019385211

---
+++ lc经典题目/20.有效的括号.c
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=c
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+char pairs(char a) {
+    if (a == '}') {
+        return '{';
+    }
+    if (a == ']') {
+        return '[';
+    }
+    if (a == ')') {
+        return '(';
+    }
+    return 0;
+}
+
+bool isValid(char * s){
+    int n = strlen(s);
+    if (n % 2 == 1) {
+        return false;
+    }
+
+    int stk[n + 1], top = 0;
+    for (int i = 0; i < n; i++) {
+        char ch = pairs(s[i]);
+        if (ch) {
+            if (top == 0 || stk[top - 1] != ch) {
+                return false;
+            }
+            top--;
+        } else {
+            stk[top++] = s[i];
+        }
+    }
+
+    return top == 0;
+}
+// @lc code=end
+
