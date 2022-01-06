dfb9bdcb499bd8cf47112bacee662ab4f52c9f5a
hs-zhangsan <1513157458@qq.com>
2022-01-07 07:54:30 +0800

    考试！！

--- lc经典题目/53.最大子数组和.c
+++ lc经典题目/53.最大子数组和.c
@@ -6,7 +6,8 @@
 
 // @lc code=start
 
-
+/*
+2022-01-07注释
 int maxSubArray(int* nums, int numsSize){
     int ret = INT_MIN;
     int sum = 0;
@@ -22,5 +23,31 @@ int maxSubArray(int* nums, int numsSize){
 
     return ret;
 }
-// @lc code=end
+*/
 
+struct Status {
+    int lSum, rSum, mSum, iSum;
+};
+
+struct Status pushUp(struct Status l, struct Status r) {
+    int iSum = l.iSum + r.iSum;
+    int lSum = fmax(l.lSum, l.iSum + r.lSum);
+    int rSum = fmax(r.rSum, r.iSum + l.rSum);
+    int mSum = fmax(fmax(l.mSum, r.mSum), l.rSum + r.lSum);
+    return (struct Status){lSum, rSum, mSum, iSum};
+};
+
+struct Status get(int* a, int l, int r) {
+    if (l == r) {
+        return (struct Status){a[l], a[l], a[l], a[l]};
+    }
+    int m = (l + r) >> 1;
+    struct Status lSub = get(a, l, m);
+    struct Status rSub = get(a, m + 1, r);
+    return pushUp(lSub, rSub);
+}
+
+int maxSubArray(int* nums, int numsSize) {
+    return get(nums, 0, numsSize - 1).mSum;
+}
+// @lc code=end
